81dc0ebf67e5d16c1f872f91685cbf1bf8b9246e
H-akim <92632018+H-akim@users.noreply.github.com>
2023-09-20 13:39:34 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var links = document.getElementsByClassName("link");
+function removeactive(){
+    for(link of links){
+        link.classList.remove("active");
+    }
+}
+for(link of links){
+    link.onclick = function(){
+        removeactive();
+        this.classList.add("active")
+    }
+}
